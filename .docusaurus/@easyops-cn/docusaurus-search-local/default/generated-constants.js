44fb7ddc4a97b8a1086635998f4a0524d792fcd0
Fab <fabian@thobe.eu>
2025-02-18 18:16:26 +0100

    Initial Commit of the Guides with Tailwind

---
+++ .docusaurus/@easyops-cn/docusaurus-search-local/default/generated-constants.js
@@ -0,0 +1,5 @@
+import lunr from "/Users/fthobe/Repositories/s3-guides/node_modules/lunr/lunr.js";
+export const removeDefaultStopWordFilter = false;
+export const language = ["en"];
+export const searchIndexUrl = "search-index{dir}.json?_=d2b947c9";
+export const searchResultLimits = 8;
---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/fthobe/Repositories/s3-guides/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/fthobe/Repositories/s3-guides/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/fthobe/Repositories/s3-guides/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/fthobe/Repositories/s3-guides/src/css/custom.css"),
+];
