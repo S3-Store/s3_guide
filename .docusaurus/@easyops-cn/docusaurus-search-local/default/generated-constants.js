f92897e22e590b2f006838704ea79f0b08f252e8
Fab <fabian@thobe.eu>
2025-04-12 18:42:26 +0200

    Update Image Size to fix DoS risk

--- .docusaurus/@easyops-cn/docusaurus-search-local/default/generated-constants.js
+++ .docusaurus/@easyops-cn/docusaurus-search-local/default/generated-constants.js
@@ -1,5 +1,5 @@
 import lunr from "/Users/fthobe/Repositories/s3_guide/node_modules/lunr/lunr.js";
 export const removeDefaultStopWordFilter = false;
 export const language = ["en"];
-export const searchIndexUrl = "search-index{dir}.json?_=a1f2a76d";
+export const searchIndexUrl = "search-index{dir}.json?_=6b51d2b1";
 export const searchResultLimits = 8;
